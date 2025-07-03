2fcce2f1b3c554ce7bc8a86e7b4a7a448c01bfff
Gringo Gang <miroslav.pecek1@gmail.com>
2025-07-03 17:47:15 +0200

    commit new test

--- src/main/java/PokusBdd.java
+++ src/main/java/PokusBdd.java
@@ -1,8 +1,36 @@
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class PokusBdd {
     @Given("nastaveni pocatecnich podminek")
     public void nastaveniPocatecnichPodminek() {
+
+    }
+
+    @And("dalsi nastavovaci krok")
+    public void dalsiNastavovaciKrok() {
+
+    }
+
+    @When("toto je nejaka akce")
+    public void totoJeNejakaAkce() {
+
+    }
+
+    @And("toto je dalsi akce")
+    public void totoJeDalsiAkce() {
+
+    }
+
+    @Then("overeni vysledku")
+    public void overeniVysledku() {
+
+    }
+
+    @And("dalsi overeni vysledku")
+    public void dalsiOvereniVysledku() {
 
     }
 }
--- src/main/resources/pokus.feature
+++ src/main/resources/pokus.feature
@@ -1,5 +1,7 @@
 Feature: pokus
 
+  #toto je komentar
+
   Scenario: Pokusny test 1
     Given nastaveni pocatecnich podminek
     And dalsi nastavovaci krok
@@ -8,4 +10,9 @@ Feature: pokus
     Then overeni vysledku
     And dalsi overeni vysledku
 
+
+    Scenario: Pokusny test 2
+      Given nastaveni pocatecnich podminek
+      When toto je dalsi akce
+      Then dalsi overeni vysledku
 
