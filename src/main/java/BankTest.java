a87483311b916bff57a3d087953bf8f1db6b7798
Gringo Gang <miroslav.pecek1@gmail.com>
2025-07-10 17:48:25 +0200

    commit new bank account

--- src/main/java/BankTest.java
+++ src/main/java/BankTest.java
@@ -1,12 +1,22 @@
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import product.BankAccount;
 
 public class BankTest {
+
+
+    BankAccount firstBankAccount;
+
+
+
+
     @Given("Bank account with {int} Kc")
-    public void bankAccountWithKc(int arg0) {
+    public void bankAccountWithKc(int initialBalance) {
+        BankAccount firstBankAccount = new BankAccount(initialBalance);
     }
 
     @Then("A user account balanc is {int} kc")
-    public void aUserAccountBalancIsKc(int arg0) {
+    public void aUserAccountBalancIsKc(int expectedBalance) {
+
     }
 }
