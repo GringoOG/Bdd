2bfe3472a7c3aa1d96d8d5062db444d3036b4df0
Gringo Gang <miroslav.pecek1@gmail.com>
2025-07-10 18:17:38 +0200

    commit withdraw

--- src/main/java/BankTest.java
+++ src/main/java/BankTest.java
@@ -1,6 +1,10 @@
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import product.BankAccount;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BankTest {
 
@@ -8,15 +12,25 @@ public class BankTest {
     BankAccount firstBankAccount;
 
 
-
-
     @Given("Bank account with {int} Kc")
     public void bankAccountWithKc(int initialBalance) {
-        BankAccount firstBankAccount = new BankAccount(initialBalance);
+         firstBankAccount = new BankAccount(initialBalance);
     }
 
     @Then("A user account balanc is {int} kc")
     public void aUserAccountBalancIsKc(int expectedBalance) {
+        assertEquals(expectedBalance, firstBankAccount.getAccountBalance());
 
+
+    }
+
+    @When("A user withdraw {int} kc")
+    public void aUserWithdrawKc(int withDrawAmount) {
+        firstBankAccount.withdraw(withDrawAmount);
+    }
+
+    @And("A user transfer {int} kc to this account")
+    public void aUserTransferKcToThisAccount(int transferAmount) {
+        firstBankAccount.transfer(transferAmount);
     }
 }
